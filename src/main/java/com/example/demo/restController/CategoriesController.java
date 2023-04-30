dc378c1efefeb757ed76cac71019538f7f8873dd
mk3 <marko.angelovski2000@yahoo.com>
2023-04-30 22:05:52 +0200

    demo

--- src/main/java/com/example/demo/restController/CategoriesController.java
+++ src/main/java/com/example/demo/restController/CategoriesController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+@CrossOrigin(origins = {"https://front-end-emt.herokuapp.com/", "https://front-end-emt.herokuapp.com/"})
 public class CategoriesController {
     private final CategoriesService categoriesService;
 
